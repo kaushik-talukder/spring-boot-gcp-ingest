2d9fd928a2f227ebc65fc23b92e30a47e0610e5b
kaushik-talukder <kaushik.talukder@gmail.com>
2020-05-24 23:26:40 -0400

    Initial commit

---
+++ src/main/java/com/ezycoding/sbgi/SpringBootGcpIngestApplication.java
@@ -0,0 +1,13 @@
+package com.ezycoding.sbgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpIngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpIngestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezycoding/sbgi/SpringBootGcpIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezycoding.sbgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGcpIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
